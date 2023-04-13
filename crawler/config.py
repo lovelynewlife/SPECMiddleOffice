17712996d6ce8a75dfe5a6ff1fb801b9fa4984f0
uw1 <newlife_1114752@163.com>
2023-04-13 05:08:30 -0700

    feat: add results data downloader.

--- crawler/config.py
+++ crawler/config.py
@@ -1,1 +1,2 @@
 DATA_ROOT_PATH = '/home/uw1/data/SPEC'
+BASE_DOMAIN = "https://spec.org/"
---
+++ crawler/download.py
@@ -0,0 +1,8 @@
+import requests
+import config
+
+
+class ResultsDownloader:
+
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
