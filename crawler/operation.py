d9aecd8af12546a86cac617bfef863ac8a57e3f7
ahoy <lovelyzcy@outlook.com>
2023-04-17 23:26:55 +0800

    feat: add singleton Launcher.

--- crawler/operation.py
+++ crawler/operation.py
@@ -1,4 +1,14 @@
-class SPEC:
+class Singleton(object):
+    _instance = None
+
+    def __new__(cls, *args, **kw):
+        if cls._instance is None:
+            orig = super(Singleton, cls)
+            cls._instance = orig.__new__(cls)
+        return cls._instance
+
+
+class Launcher(Singleton):
 
     def __init__(self):
         self.__OP = None
@@ -9,4 +19,11 @@ class SPEC:
 
     @op.setter
     def op(self, value):
-        raise PermissionError("op cannot be set.")
+        raise PermissionError("Unsupported operation")
+
+    def set_op(self):
+        self.__OP = dict()
+
+
+__SPEC = Launcher()
+SPEC = __SPEC
